b71924bf6534cd1d1ef1ffbaa6747ef2853025f6
structsize <52183019+structsize@users.noreply.github.com>
2021-09-14 21:16:42 +0900

    [INIT] init fragment example app

---
+++ fragmentexample/src/main/java/org/software/meister/gsm/fragmentexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.software.meister.gsm.fragmentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
